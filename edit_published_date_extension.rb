94b1c23e26ea7e167e23c0ccc91abe64b23a736b
Fabrice Luraine <fabrice.luraine@sofa-design.net>
2009-01-09 16:23:30 +0100

    Add additionnal extension files

---
+++ edit_published_date_extension.rb
@@ -0,0 +1,14 @@
+class EditPublishedDateExtension < Radiant::Extension
+  version "1.0"
+  description "Allows to edit the published date of a page."
+  url "http://github.com/sofadesign/radiant-edit-published-date-extension/tree/master"
+  
+  def activate
+    admin.page.edit.add('extended_metadata', 'edit_dates')
+  end
+  
+  def deactivate
+
+  end
+  
+end
