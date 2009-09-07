e78916cbd16be913161efa630082ce372be874aa
Fabrice Luraine <fabrice.luraine@sofa-design.net>
2009-09-07 10:19:50 +0200

    Updated for Radiant 0.8 (from hugomallinson)

--- edit_published_date_extension.rb
+++ edit_published_date_extension.rb
@@ -1,10 +1,10 @@
 class EditPublishedDateExtension < Radiant::Extension
-  version "1.1"
+  version "1.2"
   description "Allows to edit the published date of a page."
   url "http://github.com/sofadesign/radiant-edit-published-date-extension/tree/master"
   
   def activate
-    admin.page.edit.add('extended_metadata', 'edit_dates')
+    admin.page.edit.add :extended_metadata, 'edit_dates'
   end
   
   def deactivate
